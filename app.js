865dd8ed2c85d9592ee97fdf7d30b7299c3fd6b6
dinakajoy <dinakajoy@gmail.com>
2021-03-23 18:12:44 +0100

    update

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const compression = require('compression');
 
 const { contactFormValidationRules, validate } = require('./validators/form');
 const contactForm = require('./mail/contact');
 const app = express();
 
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
